555159bbeffb8e212faa85c104bf0f0f3383a2a0
souriguha <souriguha@gmail.com>
2014-10-28 13:58:48 +0530

    Add google analytics

--- _site/js/ga.js
+++ _site/js/ga.js
@@ -1,6 +1,6 @@
   var _gaq = _gaq || [];
-  // _gaq.push(['_setAccount', 'UA-34138258-2']);
-  // _gaq.push(['_setDomainName', 'bjacobel.com']);
+  _gaq.push(['_setAccount', 'UA-56160501-1']);
+  _gaq.push(['_setDomainName', 'reetikasachdeva.com']);
   _gaq.push(['_trackPageview']);
 
   (function() {
--- _site/readme.md
+++
@@ -1,3 +0,0 @@
-bjacobel.github.io
-=========
-My personal website and blog, hosted on GitHub Pages. Made with Jekyll and Macaw.
